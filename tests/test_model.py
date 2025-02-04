c46f1b07fc915cc4772874486a5d031ef1d05356
COSTA-ALVES Ian <ian.costa-alves@isae-supaero.fr>
2025-02-04 16:12:38 +0100

    feat(tests): add model and scenarios tests, energy and fleet are still todo

---
+++ tests/test_model.py
@@ -0,0 +1,143 @@
+from collections.abc import Callable
+from collections.abc import Mapping
+
+import pytest
+from gemseo_jax.jax_discipline import JAXDiscipline
+from gemseo_jax.jax_discipline import NumberLike
+from numpy import array
+from numpy.testing import assert_equal
+
+from noads.model import AutoModel
+from noads.model import JAXModel
+from noads.model import Model
+
+
+@pytest.fixture(scope="module")
+def function() -> Callable[[Mapping[str, NumberLike]], Mapping[str, NumberLike]]:
+    """A function computing linear combinations."""
+
+    def my_function(input_data: Mapping[str, NumberLike]):
+        """A function computing linear combinations.
+
+        Args:
+            input_data: The input data.
+
+        Returns:
+            The output data.
+        """
+        a = input_data["a"]
+        b = input_data["b"]
+        c = input_data["c"]
+        d = input_data["d"]
+
+        x = 2 * a + b - c * d
+        y = a * b
+        z = c * d
+        return {"x": x, "y": y, "z": z}
+
+    return my_function
+
+
+@pytest.fixture(scope="module")
+def input_names() -> list[str]:
+    """The input names."""
+    return ["a", "b", "c", "d"]
+
+
+@pytest.fixture(scope="module")
+def output_names() -> list[str]:
+    """The output names."""
+    return ["x", "y", "z"]
+
+
+@pytest.fixture(scope="module")
+def default_inputs() -> dict[str, NumberLike]:
+    """The default input values."""
+    # The mix of scalar, mono-dimensional and multidimensional input variables
+    # generates scalar, mono-dimensional and multidimensional variables,
+    # which allows to test the robustness of execute() and compute_jacobian().
+    return {"a": 0.0, "b": array([1.0]), "c": 2.0, "d": array([3.0, 3.0])}
+
+
+def test_model(function, input_names, output_names, default_inputs):
+    discipline = JAXDiscipline(function, input_names, output_names, default_inputs)
+    model = Model(discipline)
+
+    assert model.discipline is not None
+    assert set(model.discipline.input_grammar) == set(input_names)
+    assert set(model.discipline.output_grammar) == set(output_names)
+    for input_name in input_names:
+        assert_equal(
+            model.discipline.default_input_data[input_name], default_inputs[input_name]
+        )
+
+    output_data = model.discipline.execute()
+    default_outputs = function(default_inputs)
+
+    for output_name in output_names:
+        assert_equal(output_data[output_name], default_outputs[output_name])
+
+    assert model.discipline.check_jacobian()
+
+
+def test_jax_model(function, input_names, output_names, default_inputs):
+    model = JAXModel(function, input_names, output_names, default_inputs, "test")
+
+    assert model.discipline is not None
+    assert model.discipline.name == "test"
+    assert set(model.discipline.input_grammar) == set(input_names)
+    assert set(model.discipline.output_grammar) == set(output_names)
+    for input_name in input_names:
+        assert_equal(
+            model.discipline.default_input_data[input_name], default_inputs[input_name]
+        )
+
+    output_data = model.discipline.execute()
+    default_outputs = function(default_inputs)
+
+    for output_name in output_names:
+        assert_equal(output_data[output_name], default_outputs[output_name])
+
+    assert model.discipline.check_jacobian()
+
+
+class TestAutoModel(AutoModel):
+
+    def __init__(self):
+        super().__init__("test")
+
+    def _jax_func(self, a=0.0, b=1.0, c=2.0, d=3.0) -> tuple[float, float, float]:
+        x = 2 * a + b - c * d
+        y = a * b
+        z = c * d
+        return x, y, z
+
+
+@pytest.fixture(scope="module")
+def auto_model() -> AutoModel:
+    return TestAutoModel()
+
+
+@pytest.fixture(scope="module")
+def auto_defaults() -> dict[str, float]:
+    return {"a": 0.0, "b": 1.0, "c": 2.0, "d": 3.0}
+
+
+def test_auto_model(auto_model, function, input_names, output_names, auto_defaults):
+    assert auto_model.discipline is not None
+    assert auto_model.discipline.name == "test"
+    assert set(auto_model.discipline.input_grammar) == set(input_names)
+    assert set(auto_model.discipline.output_grammar) == set(output_names)
+    for input_name in input_names:
+        assert_equal(
+            auto_model.discipline.default_input_data[input_name],
+            auto_defaults[input_name],
+        )
+
+    output_data = auto_model.discipline.execute()
+    default_outputs = function(auto_defaults)
+
+    for output_name in output_names:
+        assert_equal(output_data[output_name], default_outputs[output_name])
+
+    assert auto_model.discipline.check_jacobian()
---
+++ tests/test_scenarios.py
@@ -0,0 +1,203 @@
+import pytest
+from gemseo_jax.jax_discipline import DataType
+from numpy import array
+
+from noads.model import AutoModel
+from noads.model import JAXModel
+from noads.scenarios.multiscenario import MultiScenario
+from noads.scenarios.temporalscenario import TemporalScenario
+
+
+class TestAutoModel(AutoModel):
+
+    def __init__(self):
+        super().__init__("auto")
+
+    def _jax_func(self, a=0.0, b=1.0, c=2.0, d=3.0) -> float:
+        x = 2 * a + b - c * d
+        return x
+
+
+def compute_yz(input_data: DataType) -> DataType:
+    a = input_data["a"]
+    b = input_data["b"]
+    c = input_data["c"]
+    d = input_data["d"]
+
+    y = a * b
+    z = c * d
+    return {"y": y, "z": z}
+
+
+class TestJAXModel(JAXModel):
+
+    def __init__(self):
+        super().__init__(
+            function=compute_yz,
+            input_names=["a", "b", "c", "d"],
+            output_names=["y", "z"],
+            default_inputs={"a": 0.0, "b": 1.0, "c": 2.0, "d": 3.0},
+            name="jax",
+        )
+
+
+@pytest.fixture(scope="module")
+def models():
+    return [TestAutoModel(), TestJAXModel()]
+
+
+@pytest.fixture(scope="module")
+def interpolated_inputs():
+    return ["b"]
+
+
+@pytest.fixture(scope="module")
+def constant_inputs():
+    return ["c"]
+
+
+@pytest.fixture(scope="module")
+def delayed_inputs():
+    return ["d"]
+
+
+@pytest.fixture(scope="module")
+def time_integrated_outputs():
+    return ["x", "z"]
+
+
+@pytest.fixture(scope="module")
+def start_year():
+    return 0.0
+
+
+@pytest.mark.parametrize(
+    ("delay_time", "interpolated_time", "end_year", "time_step", "n_inputs"),
+    [
+        (1.0, array([0.0, 5.0, 10.0]), 10.0, 1.0, 5),
+        (1.0, array([0.0, 5.0]), 10.0, 1.0, 5),
+        (1.0, array([0.0, 5.0, 10.0]), 15.0, 1.0, 5),
+        (1.0, array([0.0, 5.0, 10.0]), 10.0, 2.0, 5),
+    ]
+)
+def test_temporal_scenario(
+    models,
+    constant_inputs,
+    interpolated_inputs,
+    delayed_inputs,
+    time_integrated_outputs,
+    start_year,
+    end_year,
+    time_step,
+    delay_time,
+    interpolated_time,
+    n_inputs,
+):
+    scenario = TemporalScenario(
+        "test", models, constant_inputs, delayed_inputs, delay_time,
+        interpolated_inputs, interpolated_time, time_integrated_outputs,
+        start_year, end_year, time_step,
+    )
+
+    assert scenario.discipline is not None
+    for n in range(n_inputs):
+        input_data = {
+            name: n + n * value
+            for name, value in scenario.discipline.default_input_data.items()
+        }
+        assert scenario.discipline.check_jacobian(input_data, threshold=1e-4)
+
+    scenario.discipline.add_differentiated_inputs(["constant.c", "control.d"])
+    scenario.discipline.add_differentiated_outputs(["cumulative.x", "cumulative.z"])
+    scenario.discipline.compile_jit(pre_run=True)
+
+    for n in range(n_inputs):
+        input_data = {
+            name: n + n * value
+            for name, value in scenario.discipline.default_input_data.items()
+        }
+        assert scenario.discipline.check_jacobian(
+            input_data, threshold=1e-4,
+            # inputs=["constant.c", "control.d"],
+            # outputs=["cumulative.x", "cumulative.z"],
+        )
+
+
+@pytest.fixture(scope="module")
+def mean_outputs():
+    return ["cumulative.x", "cumulative.z"]
+
+
+@pytest.fixture(scope="module")
+def scenario_names():
+    return ["A1", "A2"]
+
+
+@pytest.fixture(scope="module")
+def fixed_inputs():
+    return ["control.d"]
+
+
+@pytest.mark.parametrize(
+    ("delay_time", "interpolated_time", "end_year", "time_step", "n_inputs"),
+    [
+        (1.0, array([0.0, 5.0, 10.0]), 10.0, 1.0, 5),
+        (1.0, array([0.0, 5.0]), 10.0, 1.0, 5),
+        (1.0, array([0.0, 5.0, 10.0]), 15.0, 1.0, 5),
+        (1.0, array([0.0, 5.0, 10.0]), 10.0, 2.0, 5),
+    ]
+)
+def test_multi_scenario(
+    mean_outputs,
+    scenario_names,
+    fixed_inputs,
+    models,
+    constant_inputs,
+    interpolated_inputs,
+    delayed_inputs,
+    time_integrated_outputs,
+    start_year,
+    end_year,
+    time_step,
+    delay_time,
+    interpolated_time,
+    n_inputs,
+):
+    temporal_scenario = TemporalScenario(
+        "test", models, constant_inputs, delayed_inputs, delay_time,
+        interpolated_inputs, interpolated_time, time_integrated_outputs,
+        start_year, end_year, time_step,
+    )
+
+    scenario = MultiScenario(
+        temporal_scenario, mean_outputs, scenario_names, fixed_inputs
+    )
+    assert scenario.discipline is not None
+    for n in range(n_inputs):
+        input_data = {
+            name: n + n * value
+            for name, value in scenario.discipline.default_input_data.items()
+        }
+        assert scenario.discipline.check_jacobian(input_data, threshold=1e-4)
+
+    scenario.discipline.add_differentiated_inputs(
+        ["A1.constant.c", "A2.constant.c", "fixed.control.d"]
+    )
+    scenario.discipline.add_differentiated_outputs(
+        ["mean.cumulative.x", "mean.cumulative.z"]
+    )
+    scenario.discipline.compile_jit(pre_run=True)
+
+    for n in range(n_inputs):
+        input_data = {
+            name: n + n * value
+            for name, value in scenario.discipline.default_input_data.items()
+        }
+        assert scenario.discipline.check_jacobian(
+            input_data, threshold=1e-4,
+            # inputs=["A1.constant.c", "A2.constant.c", "fixed.control.d"],
+            # outputs=["mean.cumulative.x", "mean.cumulative.z"],
+        )
+
+
+
